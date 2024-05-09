373d17c7eb71f8ab25df7b1f27b36fc4d2583a82
samvitakarkal <samvitaskarkal@gmail.com>
2024-05-09 09:26:14 +0530

    Adjusted code according to test cases and modularized code

--- lib/string_calculater.rb
+++ lib/string_calculater.rb
@@ -1,12 +1,4 @@
 class StringCalculator
-  def add_string(input)
-    return 'only accepts a string' unless input.is_a?(String)
-    return 'invalid input' if  input =~ /,\n/
-    negatives = ready_to_add_numbers(input).select { |num| num < 0 }
-    raise "negative numbers not allowed #{negatives}" unless negatives.empty?
-    return ready_to_add_numbers(input).sum
-  end
-
   def add(*inputs)
     if inputs.count > 1
       sum_arr = []
@@ -20,18 +12,47 @@ class StringCalculator
     end
   end
 
-  def ready_to_add_numbers(input)
-    if input =~ /\n/ && !input.start_with?("//")
-      input_array = input.split(/[\n,]/)
-    elsif input.start_with?("//")
-      delimiter_end_index = input.index("\n")
-      delimiter = input[2...delimiter_end_index]
-      input = input[(delimiter_end_index + 1)..]
-      delimiter = Regexp.escape(delimiter)
-      input_array = input.split(/#{delimiter}/) 
-    else
-      input_array = input.split(",")
-    end
-    input_array.map(&:to_i)
+  private
+
+  def add_string(input)
+    validate_input(input)
+
+    numbers = parse_input(input)
+    handle_negatives(numbers)
+
+    numbers.sum
+  end
+
+  def validate_input(input)
+    # return will not throw required error. Changed to raise
+    raise 'only accepts a string' unless input.is_a?(String)
+    raise 'invalid input' if  input =~ /,\n/
+  end
+
+  def handle_negatives(numbers)
+    negatives = numbers.select { |num| num.negative? }
+    raise "negative numbers not allowed #{negatives}" unless negatives.empty?
+  end
+
+  # broke down ready_to_add_numbers method into smaller methods
+  def parse_input(input)
+    delimiter = determine_delimiter(input)
+    input = remove_delimiter_specifier(input)
+  
+    input.split(/#{delimiter}|\n/).map(&:to_i)
+  end
+  
+  def determine_delimiter(input)
+    return ',' unless input.start_with?("//")
+  
+    delimiter_end_index = input.index("\n")
+    input[2...delimiter_end_index]
+  end
+  
+  def remove_delimiter_specifier(input)
+    return input unless input.start_with?("//")
+  
+    delimiter_end_index = input.index("\n")
+    input[(delimiter_end_index + 1)..]
   end
 end
